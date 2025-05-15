a35911f94fff0535c35d079d3c5c89847bee27ff
Văn Quý <vivanquy20804@gmail.com>
2025-05-15 23:31:56 +0700

    complete lab07.2

--- Lab07.2/app/src/main/java/com/example/quadraticequations/MainActivity.java
+++ Lab07.2/app/src/main/java/com/example/quadraticequations/MainActivity.java
@@ -1,9 +1,10 @@
-package com.example.navigatetoanotheractivity;
+package com.example.quadraticequations;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,23 +12,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ChildActivity extends AppCompatActivity {
-    Button btnBack;
+public class MainActivity extends AppCompatActivity {
+    EditText edtA, edtB;
+    Button btnResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_child);
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        btnBack = findViewById(R.id.btn_back);
-        btnBack.setOnClickListener(new View.OnClickListener() {
+        edtA = findViewById(R.id.edt_a);
+        edtB = findViewById(R.id.edt_b);
+        btnResult = findViewById(R.id.btn_result);
+        btnResult.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(ChildActivity.this, MainActivity.class));
+                Bundle bundle = new Bundle();
+                int a = Integer.parseInt(edtA.getText().toString());
+                int b = Integer.parseInt(edtB.getText().toString());
+                bundle.putInt("a", a);
+                bundle.putInt("b", b);
+                Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                intent.putExtra("mybackage", bundle);
+                startActivity(intent);
             }
         });
     }
--- Lab07.2/app/src/main/java/com/example/quadraticequations/ResultActivity.java
+++ Lab07.2/app/src/main/java/com/example/quadraticequations/ResultActivity.java
@@ -1,9 +1,10 @@
-package com.example.navigatetoanotheractivity;
+package com.example.quadraticequations;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,23 +12,43 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ChildActivity extends AppCompatActivity {
+import java.text.DecimalFormat;
+
+public class ResultActivity extends AppCompatActivity {
+    TextView txtResult;
     Button btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_child);
+        setContentView(R.layout.activity_result);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        txtResult = findViewById(R.id.txt_result);
+        Intent myintent = getIntent();
+        Bundle myBundle = myintent.getBundleExtra("mybackage");
+        int a = myBundle.getInt("a");
+        int b = myBundle.getInt("b");
+        String result = "";
+        if(a==0 && b==0){
+            result = "Vô số nghiệm";
+        }
+        else if(a==0 && b!= 0){
+            result = "Vô nghiệm";
+        }
+        else{
+            DecimalFormat dcf = new DecimalFormat("0.##");
+            result = dcf.format(-b*1.0/a);
+        }
+        txtResult.setText(result+"");
         btnBack = findViewById(R.id.btn_back);
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(ChildActivity.this, MainActivity.class));
+                finish();
             }
         });
     }
