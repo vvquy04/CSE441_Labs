cf4c63073eb96d62dba0e72a80b20207a44325ca
Văn Quý <vivanquy20804@gmail.com>
2025-05-16 18:19:25 +0700

    push code lab11

---
+++ Lab11_WebView/app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.webview;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Intent intent = getIntent();
+        Uri data  = intent.getData();
+        try{
+            @SuppressLint("WrongViewCast") WebView webView = findViewById(R.id.webview1);
+            webView.loadUrl(data.toString());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
